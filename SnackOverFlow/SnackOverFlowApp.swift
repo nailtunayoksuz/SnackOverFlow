296f30cf9f60232ad5ce680f88cf112766d8b33c
Nail Tunay ÖKSÜZ <nailtunayoksuz@hotmail.com>
2022-11-23 21:58:56 +0300

    Initial Commit

---
+++ SnackOverFlow/SnackOverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackOverFlowApp.swift
+//  SnackOverFlow
+//
+//  Created by Nail Tunay ÖKSÜZ on 23.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SnackOverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
